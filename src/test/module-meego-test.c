f2014a85d2290296e4e5d697e3519086682e5f8b
Juho Hämäläinen <ext-juho.hamalainen@nokia.com>
2010-09-07 12:59:14 +0300

    test: add sink-input volume tracking and proplist setter

--- src/test/module-meego-test.c
+++ src/test/module-meego-test.c
@@ -38,6 +38,7 @@
 #include <pulsecore/modargs.h>
 #include <pulsecore/namereg.h>
 #include <pulsecore/log.h>
+#include <pulsecore/core-util.h>
 
 #include "src/common/proplist-meego.h"
 
@@ -45,29 +46,179 @@
 
 PA_MODULE_AUTHOR("Pekka Ervasti");
 PA_MODULE_DESCRIPTION("test module");
-PA_MODULE_USAGE("sink_name=<name of hw sink> audio_mode=<ihf,hs> hwid=<accessory hwid>");
+PA_MODULE_USAGE(
+        "op=<test operation, mode/si/proplist> "
+        "sink_name=<name of hw sink> "
+        "audio_mode=<ihf,hs,etc> "
+        "hwid=<accessory hwid> "
+        "property=<property key to change> "
+        "value=<property value to change>");
 PA_MODULE_VERSION(PACKAGE_VERSION);
 
 static const char* const valid_modargs[] = {
+    "op",
     "sink_name",
     "audio_mode",
     "hwid",
+    "property",
+    "value",
     NULL,
 };
+
+#define OP_AUDIO_MODE "mode"
+#define OP_SINK_INPUT "si"
+#define OP_PROPLIST "proplist"
 
 struct userdata {
     pa_core *core;
     pa_module *module;
+
+    /* for sink-input test */
+    pa_subscription *subscription;
 };
 
-int pa__init(pa_module*m) {
-    pa_modargs *ma = NULL;
-    struct userdata *u;
+/* copy/pasted from module-stream-restore.c */
+#define IDENTIFICATION_PROPERTY "module-stream-restore.id"
+static char *get_name(pa_proplist *p, const char *prefix) {
+    const char *r;
+    char *t;
+
+    if (!p)
+        return NULL;
+
+    if ((r = pa_proplist_gets(p, IDENTIFICATION_PROPERTY)))
+        return pa_xstrdup(r);
+
+    if ((r = pa_proplist_gets(p, PA_PROP_MEDIA_ROLE)))
+        t = pa_sprintf_malloc("%s-by-media-role:%s", prefix, r);
+    else if ((r = pa_proplist_gets(p, PA_PROP_APPLICATION_ID)))
+        t = pa_sprintf_malloc("%s-by-application-id:%s", prefix, r);
+    else if ((r = pa_proplist_gets(p, PA_PROP_APPLICATION_NAME)))
+        t = pa_sprintf_malloc("%s-by-application-name:%s", prefix, r);
+    else if ((r = pa_proplist_gets(p, PA_PROP_MEDIA_NAME)))
+        t = pa_sprintf_malloc("%s-by-media-name:%s", prefix, r);
+    else
+        t = pa_sprintf_malloc("%s-fallback:%s", prefix, r);
+
+    pa_proplist_sets(p, IDENTIFICATION_PROPERTY, t);
+    return t;
+}
+
+static void test_sink_input_subscribe_cb(pa_core *c, pa_subscription_event_type_t t, uint32_t idx, void *userdata) {
+    struct userdata *u = userdata;
+    pa_sink_input *si = NULL;
+    char *name;
+    char tmp[256];
+    pa_cvolume absolute_volume;
+    pa_cvolume reference_ratio;
+
+    if (t != (PA_SUBSCRIPTION_EVENT_SINK_INPUT|PA_SUBSCRIPTION_EVENT_NEW) &&
+        t != (PA_SUBSCRIPTION_EVENT_SINK_INPUT|PA_SUBSCRIPTION_EVENT_CHANGE))
+        return;
+
+    if (!(si = pa_idxset_get_by_index(c->sink_inputs, idx)))
+        return;
+
+    if (!(name = get_name(si->proplist, "sink-input")))
+        return;
+
+    pa_sink_input_get_volume(si, &reference_ratio, FALSE);
+    pa_sink_input_get_volume(si, &absolute_volume, TRUE);
+
+    pa_log_debug("sink-input: %s reference ratio: %s absolute volume: %s",
+            name,
+            pa_cvolume_snprint(tmp, sizeof(tmp), &reference_ratio),
+            pa_cvolume_snprint(tmp, sizeof(tmp), &absolute_volume));
+
+    pa_xfree(name);
+}
+
+static void test_sink_input(struct userdata *u) {
+    u->subscription = pa_subscription_new(u->core, PA_SUBSCRIPTION_MASK_SINK_INPUT, test_sink_input_subscribe_cb, u);
+    pa_log_debug("Setting up subscription for sink-input");
+}
+
+static void test_proplist(struct userdata *u, pa_modargs *ma) {
+    const char *sink_name;
+    const char *property;
+    const char *value;
+    pa_sink *s;
+    pa_proplist *p;
+
+    sink_name = pa_modargs_get_value(ma, "sink_name", NULL);
+    s = pa_namereg_get(u->core, sink_name, PA_NAMEREG_SINK);
+
+    if (!s) {
+        pa_log("No such sink: %s", sink_name);
+        return;
+    }
+
+    pa_sink_ref(s);
+
+    property = pa_modargs_get_value(ma, "property", NULL);
+    value = pa_modargs_get_value(ma, "value", NULL);
+
+    if (!property) {
+        pa_log("No property defined");
+        return;
+    }
+    if (!value) {
+        pa_log("No value defined!");
+        return;
+    }
+
+    p = pa_proplist_new();
+
+    pa_proplist_sets(p, property, value);
+
+    pa_proplist_update(s->proplist, PA_UPDATE_REPLACE, p);
+    pa_log_debug("for sink %s set %s = %s", s->name, property, value);
+
+    if (PA_SINK_IS_LINKED(s->state)) {
+        pa_hook_fire(&s->core->hooks[PA_CORE_HOOK_SINK_PROPLIST_CHANGED], s);
+        pa_subscription_post(s->core, PA_SUBSCRIPTION_EVENT_SINK|PA_SUBSCRIPTION_EVENT_CHANGE, s->index);
+    }
+
+    pa_proplist_free(p);
+    pa_sink_unref(s);
+
+    pa_module_unload_request(u->module, TRUE);
+}
+
+static void test_audio_mode(struct userdata *u, pa_modargs *ma) {
     const char *sink_name;
     const char *audio_mode;
     const char *hwid;
     pa_sink *hw_sink;
     pa_proplist *p;
+
+    sink_name = pa_modargs_get_value(ma, "sink_name", NULL);
+    hw_sink = pa_namereg_get(u->core, sink_name, PA_NAMEREG_SINK);
+
+    if (!hw_sink) {
+        pa_log("No such sink: %s", sink_name);
+        return;
+    }
+
+    audio_mode = pa_modargs_get_value(ma, "audio_mode", "ihf");
+    hwid = pa_modargs_get_value(ma, "hwid", "");
+
+    p = pa_proplist_new();
+
+    pa_proplist_sets(p, PA_NOKIA_PROP_AUDIO_MODE, audio_mode);
+    pa_proplist_sets(p, PA_NOKIA_PROP_AUDIO_ACCESSORY_HWID, hwid);
+
+    pa_sink_update_proplist(hw_sink, PA_UPDATE_REPLACE, p);
+    pa_proplist_free(p);
+
+    /* Work done, let's unload */
+    pa_module_unload_request(u->module, TRUE);
+}
+
+int pa__init(pa_module*m) {
+    pa_modargs *ma = NULL;
+    struct userdata *u;
+    const char *op;
 
     u = pa_xmalloc(sizeof(struct userdata));
 
@@ -82,29 +233,20 @@ int pa__init(pa_module*m) {
     u->core = m->core;
     u->module = m;
 
-    sink_name = pa_modargs_get_value(ma, "sink_name", NULL);
-    hw_sink = pa_namereg_get(m->core, sink_name, PA_NAMEREG_SINK);
-
-    if (!hw_sink) {
-        pa_log("No such sink: %s", sink_name);
+    op = pa_modargs_get_value(ma, "op", NULL);
+    if (!op) {
+        pa_log("Operation not defined!");
         goto fail;
     }
 
-    audio_mode = pa_modargs_get_value(ma, "audio_mode", "ihf");
-    hwid = pa_modargs_get_value(ma, "hwid", "");
-
-    p = pa_proplist_new();
-
-    pa_proplist_sets(p, PA_NOKIA_PROP_AUDIO_MODE, audio_mode);
-    pa_proplist_sets(p, PA_NOKIA_PROP_AUDIO_ACCESSORY_HWID, hwid);
-
-    pa_sink_update_proplist(hw_sink, PA_UPDATE_REPLACE, p);
-    pa_proplist_free(p);
+    if (pa_streq(op, OP_AUDIO_MODE))
+        test_audio_mode(u, ma);
+    else if (pa_streq(op, OP_SINK_INPUT))
+        test_sink_input(u);
+    else if (pa_streq(op, OP_PROPLIST))
+        test_proplist(u, ma);
 
     pa_modargs_free(ma);
-
-    /* Work done, let's unload */
-    pa_module_unload_request(m, TRUE);
 
     return 0;
 
