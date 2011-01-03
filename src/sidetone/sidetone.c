d4815bcce8fbfcb6ec288786bf6fea4139736e98
Sudarshan <sudarshan.bisht@nokia.com>
2011-01-03 16:45:01 +0200

    sidetone: return when current master volume is same as previous. sidetone: when master sink volume is mute then better to mute the sidetone also.

--- src/sidetone/sidetone.c
+++ src/sidetone/sidetone.c
@@ -41,7 +41,6 @@
 #include "sidetone-args.h"
 #include "sidetone.h"
 
-/* we have only 7 steps in Left Digital Loopback element i.e. sidetone control element*/
 
 /* Describes the volume of an element in the sideone path. */
 struct element_volume {
@@ -76,45 +75,49 @@ struct sidetone {
     struct mv_volume_steps *total_steps;
     /* step used for setting sidetone control element volume */
     int sidetone_step;
-
 };
 
 
 
-static int sidetone_volume_get_step(struct sidetone *st){
-    pa_assert(st->volume_current);
+static int sidetone_volume_get_step(struct sidetone *st) {
     pa_cvolume *cvol;
     pa_volume_t volume;
     int volume_mb;
-    int i=0;
+    int i = 0;
+
+    pa_assert(st);
+    pa_assert(st->volume_current);
 
     cvol = &st->master_sink->real_volume;
 
-    if(pa_cvolume_equal(cvol, st->volume_current)){
-        pa_log_debug("volume is same as previous one");
-        return -1;
-    }
+    /* if volume is same as previous one, then no need to repeat the things */
+    if (pa_cvolume_equal(cvol, st->volume_current))
+        return st->sidetone_step;
 
     st->volume_current->channels = cvol->channels;
-    for(i = 0; i < st->volume_current->channels; i++){
+    for (i = 0; i < st->volume_current->channels; i++)
         st->volume_current->values[i] = cvol->values[i];
-    }
 
     volume = pa_cvolume_avg(cvol);
     pa_log_debug("volume %d from sink %s", volume, st->master_sink->name);
+    if (volume == PA_VOLUME_MUTED) {
+        pa_log_debug("master sink volume is muted, better mute sidetone");
+        return 0;
+    }
 
-    volume_mb=(int)(pa_sw_volume_to_dB(volume) * 100 - 0.5);
+    volume_mb = (int) (pa_sw_volume_to_dB(volume) * 100);
     pa_log_debug("volume %d millibels ", volume_mb);
 
     /* Search for the first step that's >= volume_mb.
      * Assume that the steps are in ascending order. */
-    for(i = 0; (i < st->total_steps->n_steps) && (st->total_steps->step[i] < volume_mb); i++);
+    for (i = 0; (i < st->total_steps->n_steps) && (st->total_steps->step[i] < volume_mb); i++)
+        ;
 
-    if(i == st->total_steps->n_steps)
+    if (i == st->total_steps->n_steps)
         i = st->total_steps->n_steps - 1;
 
     /* If we're in between steps, choose the closer one. With equal distances, choose the higher step. */
-    if(i > 0 && (st->total_steps->step[i] - volume_mb > volume_mb - st->total_steps->step[i - 1]))
+    if (i > 0 && (st->total_steps->step[i] - volume_mb > volume_mb - st->total_steps->step[i - 1]))
         i--;
 
     st->sidetone_step = st->total_steps->index[i];
@@ -130,29 +133,36 @@ step */
 static void sink_input_subscribe_sidetone_cb(pa_core *c, pa_subscription_event_type_t t, uint32_t idx, sidetone *st) {
     int sidetone_step = -1;
     int ret = 0;
+    int old_sidetone_step;
+
     pa_assert(c);
     pa_assert(st);
 
     pa_log_debug("subscription event is called  ");
 
-    if (PA_SUBSCRIPTION_EVENT_SINK_INPUT != (t & PA_SUBSCRIPTION_EVENT_FACILITY_MASK)){
+    if (PA_SUBSCRIPTION_EVENT_SINK_INPUT != (t & PA_SUBSCRIPTION_EVENT_FACILITY_MASK)) {
         pa_log_debug("subscription event not found");
         return;
     }
 
+    old_sidetone_step = st->sidetone_step;
     /* fetch the sidetone step */
     sidetone_step = sidetone_volume_get_step(st);
-    if(sidetone_step < 0){
+    if (sidetone_step == old_sidetone_step ) {
+            pa_log_debug("volume is same as previous one");
+            return;
+    }
+    if (sidetone_step < 0 ) {
         pa_log_warn("cant fetch the correct sidetone volume step");
+        return;
     }
 
     pa_log_debug(" setting sidetone step %d", sidetone_step);
 
     /* set the sidetone volume using step */
     ret = set_ctrl_element_volume(st->ctrl_element, sidetone_step);
-    if(ret < 0){
+    if (ret < 0)
        pa_log_warn("can't set sidetone volume");
-    }
 
 }
 
@@ -177,7 +187,7 @@ static pa_hook_result_t sink_unlink_cb(pa_sink *sink, void *call_data, sidetone 
     pa_log_debug(" sink_unlink_cb");
     pa_mutex_lock(st->mutex);
 
-    if(sink == st->master_sink) {
+    if (sink == st->master_sink) {
         pa_log_warn("An important sink (%s) was unlinked. Disabling sidetone module.", sink->name);
         set_dead(st);
     }
@@ -191,17 +201,16 @@ static pa_hook_result_t sink_unlink_cb(pa_sink *sink, void *call_data, sidetone 
 
 /* Create a new sidetone instance. argument contains the raw module args of the sidetone module */
 sidetone *sidetone_new(pa_core *core, const char* argument) {
-    pa_assert(core);
-    pa_assert(argument);
     sidetone *st = NULL;
     sidetone_args *st_args = NULL;
     pa_sink *sink = NULL;
     int i = 0;
 
+    pa_assert(core);
+    pa_assert(argument);
 
-    if(!(st_args = sidetone_args_new(argument))) {
+    if (!(st_args = sidetone_args_new(argument)))
         goto fail;
-    }
 
     st = pa_xnew0(struct sidetone, 1);
     st->volume_current = pa_xnew0(struct pa_cvolume, 1);;
@@ -209,25 +218,25 @@ sidetone *sidetone_new(pa_core *core, const char* argument) {
     st->total_steps = pa_xnew0(struct mv_volume_steps, 1);
     st->total_steps->n_steps = st_args->steps->n_steps;
     st->total_steps->current_step = st_args->steps->current_step;
-    for(i = 0; i < st->total_steps->n_steps; i++){
+    for (i = 0; i < st->total_steps->n_steps; i++) {
         st->total_steps->step[i] = st_args->steps->step[i];
         st->total_steps->index[i] = st_args->steps->index[i];
     }
 
     st->mutex = pa_mutex_new(FALSE, FALSE);
 
-    if(!(st->mixer = pa_alsa_old_open_mixer(st_args->mixer))) {
+    if (!(st->mixer = pa_alsa_old_open_mixer(st_args->mixer))) {
         pa_log_error("Failed to open mixer \"%s\"", st_args->mixer);
         goto fail;
     }
 
-   if(!(st->ctrl_element = ctrl_element_new(st->mixer, st_args->control_element))) {
+    if (!(st->ctrl_element = ctrl_element_new(st->mixer, st_args->control_element))) {
         pa_log_error("Failed to create control element");
         goto fail;
     }
 
     /* there should be at least one sink */
-    if(!(sink = pa_namereg_get(core, st_args->master_sink, PA_NAMEREG_SINK))) {
+    if (!(sink = pa_namereg_get(core, st_args->master_sink, PA_NAMEREG_SINK))) {
         pa_log_error("Sink %s not found", st_args->master_sink);
         goto fail;
     }
@@ -251,10 +260,10 @@ sidetone *sidetone_new(pa_core *core, const char* argument) {
 
 fail:
 
-    if(st_args)
+    if (st_args)
         sidetone_args_free(st_args);
 
-    if(st)
+    if (st)
         sidetone_free(st);
 
     return NULL;
@@ -263,37 +272,37 @@ fail:
 void sidetone_free(sidetone *st) {
     pa_assert(st);
 
-    if(st->ctrl_element){
+    if (st->ctrl_element) {
       ctrl_element_mute(st->ctrl_element);
       st->ctrl_element = NULL;
     }
 
-    if (st->mixer){
+    if (st->mixer) {
         snd_mixer_close(st->mixer);
         st->mixer = NULL;
     }
 
-    if(st->total_steps){
+    if (st->total_steps) {
         pa_xfree(st->total_steps);
         st->total_steps = NULL;
     }
 
-    if(st->volume_current){
+    if (st->volume_current) {
         pa_xfree(st->volume_current);
         st->volume_current = NULL;
     }
 
-    if(st->sink_subscription){
+    if (st->sink_subscription) {
         pa_subscription_free(st->sink_subscription);
         st->sink_subscription = NULL;
     }
 
-    if(st->sink_unlink_slot){
+    if (st->sink_unlink_slot) {
         pa_hook_slot_free(st->sink_unlink_slot);
         st->sink_unlink_slot = NULL;
     }
 
-    if(st->mutex){
+    if (st->mutex) {
         pa_mutex_free(st->mutex);
         st->mutex = NULL;
     }
