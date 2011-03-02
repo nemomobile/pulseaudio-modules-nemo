2729fbd05de67f1677a17555b01c61ba23012ca3
Juho Hämäläinen <ext-juho.hamalainen@nokia.com>
2011-03-02 11:16:45 +0200

    voice: update api definitions a bit

--- src/voice/module-voice-api.h
+++ src/voice/module-voice-api.h
@@ -41,20 +41,21 @@
 
 #define VOICE_API_VERSION "0.1"
 
-#define VOICE_HOOK_HW_SINK_PROCESS              "x-meego.voice.hw_sink_process"
-#define VOICE_HOOK_NARROWBAND_EAR_EQU_MONO      "x-meego.voice.narrowband_ear_equ_mono"
-#define VOICE_HOOK_NARROWBAND_MIC_EQ_MONO       "x-meego.voice.narrowband_mic_eq_mono"
-#define VOICE_HOOK_WIDEBAND_MIC_EQ_MONO         "x-meego.voice.wideband_mic_eq_mono"
-#define VOICE_HOOK_WIDEBAND_MIC_EQ_STEREO       "x-meego.voice.wideband_mic_eq_stereo"
-#define VOICE_HOOK_XPROT_MONO                   "x-meego.voice.xport_mono"
-#define VOICE_HOOK_VOLUME                       "x-meego.voice.volume"
-#define VOICE_HOOK_CALL_VOLUME                  "x-meego.voice.call_volume"
-#define VOICE_HOOK_CALL_BEGIN                   "x-meego.voice.call_begin"
-#define VOICE_HOOK_CALL_END                     "x-meego.voice.call_end"
-#define VOICE_HOOK_AEP_DOWNLINK                 "x-meego.voice.aep_downlink"
-#define VOICE_HOOK_AEP_UPLINK                   "x-meego.voice.aep_uplink"
-#define VOICE_HOOK_RMC_MONO                     "x-meego.voice.rmc"
-#define VOICE_HOOK_SOURCE_RESET                 "x-meego.voice.source_reset"
+/*      C-name                                  hook name                                   call_data */
+#define VOICE_HOOK_HW_SINK_PROCESS              "x-meego.voice.hw_sink_process"         /* default 2ch */
+#define VOICE_HOOK_NARROWBAND_EAR_EQU_MONO      "x-meego.voice.narrowband_ear_equ_mono" /* default 1ch */
+#define VOICE_HOOK_NARROWBAND_MIC_EQ_MONO       "x-meego.voice.narrowband_mic_eq_mono"  /* default 1ch */
+#define VOICE_HOOK_WIDEBAND_MIC_EQ_MONO         "x-meego.voice.wideband_mic_eq_mono"    /* default 1ch */
+#define VOICE_HOOK_WIDEBAND_MIC_EQ_STEREO       "x-meego.voice.wideband_mic_eq_stereo"  /* default 2ch */
+#define VOICE_HOOK_XPROT_MONO                   "x-meego.voice.xprot_mono"              /* default 1ch */
+#define VOICE_HOOK_VOLUME                       "x-meego.voice.volume"                  /* pa_cvolume */
+#define VOICE_HOOK_CALL_VOLUME                  "x-meego.voice.call_volume"             /* pa_cvolume */
+#define VOICE_HOOK_CALL_BEGIN                   "x-meego.voice.call_begin"              /* NULL */
+#define VOICE_HOOK_CALL_END                     "x-meego.voice.call_end"                /* NULL */
+#define VOICE_HOOK_AEP_DOWNLINK                 "x-meego.voice.aep_downlink"            /* aep_downlink */
+#define VOICE_HOOK_AEP_UPLINK                   "x-meego.voice.aep_uplink"              /* aep_uplink */
+#define VOICE_HOOK_RMC_MONO                     "x-meego.voice.rmc"                     /* default 1ch */
+#define VOICE_HOOK_SOURCE_RESET                 "x-meego.voice.source_reset"            /* NULL */
 
 
 typedef struct {
